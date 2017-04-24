62095ca88c433443100cff55765043b3253e38b1
DongHyunKims <eldk1229@gmail.com>
2017-04-24 19:36:27 +0900

    Add 폴더 구조 생성
    jinniecast project의 폴더 구조 생성

---
+++ mainApp/src/App.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="mainHeader">
+        </header>
+
+        <div className="container">
+
+            <div className="leftArea">
+              left
+            </div>
+
+            <div className="rightArea">
+              right
+            </div>
+
+            <nav className="navArea">
+            </nav>
+
+        </div>
+
+
+        <footer className="mainFooter">
+        </footer>
+
+      </div>
+    );
+  }
+}
+
+export default App;
