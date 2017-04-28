ed5d046c3a6592ffbd3de6c3523b497f557ec1aa
kimseyoon <kimse7533@naver.com>
2017-04-28 17:41:35 +0900

    Updata duration totalViews 데이터 가져오기
    1. viewCount, duration 가져오기
    2. utube DATA API에서 받아온 값중에 쓰는 값들만 따로 저장

--- mainApp/src/components/mainListComponent/SearchList.js
+++ mainApp/src/components/mainListComponent/SearchList.js
@@ -8,23 +8,33 @@ class SearchList extends React.Component{
   }
 
   makeListItem(items){
-    return items.map((data) => {
+    return items.map((data, index) => {
       //console.log(data)
-      return <SearchListItem data={data}/>
+      return <SearchListItem
+              data={data}
+              key={data.videoId}
+              index={index}
+              clickAddButton={this.props.clickAddButton}
+            />
     })
   }
 
   render(){
     return(
-      <ul className="searchList">
-        {this.makeListItem(this.props.items)}
-      </ul>
+      <div className="searchListWrap">
+        <ul className="searchList">
+          {this.makeListItem(this.props.items)}
+        </ul>
+        <button onClick={this.props.moreVideoList} className="moreBtn">더읽어들이기</button>
+      </div>
     )
   }
 }
 
 SearchList.propTypes = {
-  items : React.PropTypes.array
+  items : React.PropTypes.array,
+  clickAddButton : React.PropTypes.func,
+  moreVideoList : React.PropTypes.func
 }
 
 export default SearchList;
