0ba692b39593283a034a01d9710dea337d033e79
DongHyunKims <eldk1229@gmail.com>
2017-05-01 00:35:06 +0900

    Modify PlayList.js,YoutubePlayer.js
    1. PlayList.js : playList에서 선택된 동영상이 있는지 없는지 확인
    2. YoutubePlayer.js : iframeAPI 사용.

--- mainApp/src/components/playListComponent/YoutubePlayer.js
+++ mainApp/src/components/playListComponent/YoutubePlayer.js
@@ -3,74 +3,172 @@
  */
 
 import React, {Component} from 'react';
-import load from 'load-script';
+import loadScript from 'load-script';
 
 
 
 const SDK_URL = 'https://www.youtube.com/iframe_api';
 const SDK_GLOBAL = 'YT';
+const SDK_GLOBAL_READY = 'onYouTubeIframeAPIReady';
 
-const DEFAULT_PLAYER_VARS = {
-    autoplay: 0,
-    playsinline: 1,
-    showinfo: 0,
-    rel: 0,
-    iv_load_policy: 3
-}
+//
+// const DEFAULT_PLAYER_VARS = {
+//     autoplay: 0,
+//     playsinline: 1,
+//     showinfo: 0,
+//     rel: 0,
+//     iv_load_policy: 3
+// };
 
 class YoutubePlayer extends Component{
 
     constructor(props){
         super(props);
+        this.state = {
+            player: null,
+            playerState : -1,
+            isReady: false,
+        };
+
+        this.ref = this.ref.bind(this);
+        this.load = this.load.bind(this);
+        this.getSDK = this.getSDK.bind(this);
+        this.onPlayerReady = this.onPlayerReady.bind(this);
+    }
+
+    componentDidMount(){
+        this.load();
+
+    }
 
 
-        load(SDK_URL, function (err, script) {
-            if (err) {
-                // print useful message
-            }
-            else {
-                console.log("fasfasdfasd",script.src);// Prints 'foo'.js'
-                // use script
-                // note that in IE8 and below loading error wouldn't be reported
-            }
-        })
+    getSDK(){
+
+        // if (window[SDK_GLOBAL] && window[SDK_GLOBAL].loaded) {
+        //     return Promise.resolve(window[SDK_GLOBAL]);
+        // }
+
+        return new Promise((resolve,reject)=>{
+            //onYouTubeIframeAPIReady 함수는 플레이어 API 코드가 다운로드되는 즉시 실행됩니다.
+            //onYouTubeIframeAPIReady함수는 global scope에 존재 해야 합니다
+            window[SDK_GLOBAL_READY] = function () {
+                //console.log("previousOnReady",previousOnReady);
+                //if (previousOnReady) previousOnReady();
+                resolve(window[SDK_GLOBAL]);
+            };
+
+            loadScript(SDK_URL, err => {
+                if (err) reject(err)
+            });
+            //  YT: window[SDK_GLOBAL],
+
+        });
     }
-    //youtube url 생성 메소드
-    createYoutubeUrl(src){
-        let opts = this.props.opts;
-        return Object.keys(opts).reduce((pre,post)=>{
-            let opt = "&" + post + "=" + opts[post];
-            return pre + opt;
-        },src);
+
+    load(){
+        this.getSDK().then((YT)=>{
+            let player = new YT.Player(this.container, {
+                height: '100%',
+                width: '100%',
+                events:{
+                    onReady: this.onPlayerReady.bind(null,'M7lc1UVf-VE')
+                }
+            });
+
+            this.setState({player: player});
+
+        }).catch((err)=>{
+            console.log(err);
+        });
+
     }
+
+    onPlayerReady(videoId) {
+        //player.loadVideoById(videoId);
+        let { player } = this.state;
+        player.loadVideoById(videoId);
+        //player.playVideo();
+        this.setState({ isReady : true });
+
+    }
+
+
+
+    // //youtube url 생성 메소드
+    // createYoutubeUrl(src){
+    //     let opts = this.props.opts;
+    //     return Object.keys(opts).reduce((pre,post)=>{
+    //         let opt = "&" + post + "=" + opts[post];
+    //         return pre + opt;
+    //     },src);
+    // }
+
+
+    ref(container){
+        this.container = container
+    }
+
 
     render() {
         let {styles,videoId} = this.props;
-        let src = "https://www.youtube.com/embed/" + videoId + "?";
-        src = this.createYoutubeUrl(src);
-        if(!styles){
-            styles = {
-                width: "100%",
-                height: "100%",
-            }
+        let { player,isReady } = this.state;
+
+        if(isReady && videoId){
+            player.loadVideoById(videoId);
         }
 
 
-        let renderingIframe = <div>Loading...</div>;
 
-        if(videoId){
-            renderingIframe =  <iframe style={styles} src={src} frameBorder="0" allowFullScreen ></iframe>;
-        }
+        // let url = null;
+        // if(videoId) {
+        //     url = "http://www.youtube.com/v/" + videoId + "?version=3";
+        //     player.cueVideoById(videoId);
+        // }
+        //
+        //
+        // if(window[SDK_GLOBAL_READY]){
+        //     player.loadVideoById(videoId);
+        // }
+
+       // let renderingIframe = <div>Loading...</div>;
+       //  console.log("fff","3");
+       //  console.log("player",player);
+       //  if(player) {
+       //
+       //      player.loadVideoById(videoId);
+       //  }
+
+
+        //console.log("2");
+
+        // let src = "https://www.youtube.com/embed/" + videoId + "?";
+        // src = this.createYoutubeUrl(src);
+        // if(!styles){
+        //     styles = {
+        //         width: "100%",
+        //         height: "100%",
+        //     }
+        // }
+
+
+
+
+        // if(videoId){
+        //     renderingIframe =  <iframe style={styles} src={src} frameBorder="0" allowFullScreen ></iframe>;
+        // }
+
+        //{renderingIframe}
 
         //controls=0
         //autoplay=1
         //playlist=XGSy3_Czz8k&loop=1
         //?autoplay=1&rel=0&enablejsapi=1&frameborder=0&allowfullscreen
 
+
+
+
         return (
-            <div className="youtubePlayerArea">
-                {renderingIframe}
-            </div>
+            <div ref={this.ref} />
         );
     }
 
