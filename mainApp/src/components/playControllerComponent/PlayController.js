28e6b271bc11682b13375d92d06c356c41503681
sujinleeme <sujinlee.me@gmail.com>
2017-05-01 14:57:38 +0900

    Add autoplay when user clicks next / prev button

--- mainApp/src/components/playControllerComponent/PlayController.js
+++ mainApp/src/components/playControllerComponent/PlayController.js
@@ -3,16 +3,14 @@
  */
 import React from 'react';
 import YouTube from './Youtube';
+import ReactDOM from 'react-dom';
 import "./playController.css"
 
 // 이전 버튼, 이후 버튼, 플레이리스트 내 컴포넌트를 클릭할 때마다 prev, current, next Video ID를 업데이트할 수 있는 메소드가 필요.
 const selectedVideo = {id : {prev: 'XNoMw1Dmqzs', current: '-DX3vJiqxm4', next:'MmKlaGpmYig'}}
 
 function contentClass(isShow) {
-  if (isShow) {
-    return "invisible";
-  }
-  return "visible";
+  isShow ? "invisible" : "visible"
 }
 
 class PlayController extends React.Component {
@@ -22,87 +20,99 @@ class PlayController extends React.Component {
     this.state = {
       videoId: selectedVideo.id.current,
       player: null,
-      event_map: { play: false, //재생 false 일경우 play는 pause가 된다.  
+      event_map: { playing: false,
                    curTime: null, // 현재 재생 시간
                    totalTime: null, // 전체 비디오 재생 시간 
                    volumeChange: null // 볼륨 조절
                   }
     };
 
+    this.opts = {
+      videoId: this.state.videoId,
+      height: '390',
+      width: '640',
+      playerVars: { // https://developers.google.com/youtube/player_parameters
+        autoplay: 0
+      }
+    };
+    
     this.onReady = this.onReady.bind(this);
     this.onChangePrevVideo = this.onChangePrevVideo.bind(this);
     this.onChangeNextVideo = this.onChangeNextVideo.bind(this);
     this.onPlayVideo = this.onPlayVideo.bind(this);
     this.onPauseVideo = this.onPauseVideo.bind(this);
     this.onEndVideo = this.onEndVideo.bind(this);
-    // this.onStateChange = this.onStateChange.bind(this);
+    this.getDuration = this.getDuration.bind(this);
   }
-
+ 
   onReady(event) {
     console.log(`재생 될 비디오 아이디 : "${this.state.videoId}"`);
+    this.setState({ player: event.target });
+    this.state.player ? this.getDuration() : null
+    console.log("재생 될 비디오 아이디", this.state.event_map.totalTime);
+  }
+
+  setDuration() {
     this.setState({
-      player: event.target,
-    });
+      event_map: Object.assign({}, this.state.event_map, {
+        totalTime: this.state.player.getDuration()
+      }),
+    }); 
   }
 
   onPlayVideo() {
+    this.opts.playerVars.autoplay = 1;
     this.state.player.playVideo();
-    const event_map = this.state.event_map;
-    event_map.play = !event_map.play; 
-    // event_map.timeupdate = this.state.player.getCurrentTime();
-    this.setState({event_map});
-    
+    if(YouTube.PlayerState.PLAYING) {
+      this.setState({
+         event_map: Object.assign({}, this.state.event_map, { playing: true }),
+      });
+      setTimeout(() => {
+        this.setDuration();
+        console.log('현재 재생시간', this.state.videoId, this.state.event_map.playing, this.state.event_map.totalTime);
+      }, 1500);
+    }
   }
 
   onPauseVideo() {
+     this.setState({
+      event_map: Object.assign({}, this.state.event_map, { playing: false }),
+    });
     this.state.player.pauseVideo();
-    const event_map = this.state.event_map;
-    event_map.play = !event_map.play; 
-    this.setState({event_map});
+    
   }
 
   onEndVideo() {
     this.state.player.endVideo();
   }
-  //--- 현재 재생 시간 입력 
-  // componentDidMount() {
-  //   setInterval( () => {
-  //     const event_map = this.state.event_map;
-  //     event_map.curTime = this.state.player.getCurrentTime();
-  //     this.setState({
-  //       event_map
-  //     })
-  //   },100);
-  //   console.log(this.state.event_map.curTime);
-  // }
-
+ 
   // 다음 비디오로 이동
   onChangeNextVideo() {
-    this.setState({
-      videoId: selectedVideo.id.next,
-    });
+    this.setState({ videoId: selectedVideo.id.next });
+    setTimeout(() => { this.onPlayVideo(); }, 500);
   }
 
   // 이전 비디오로 이동
   onChangePrevVideo() {
-    this.setState({
-      videoId: selectedVideo.id.prev,
-    });
+    this.setState({ videoId: selectedVideo.id.prev });
+
+    setTimeout(() => {
+      this.onPlayVideo();
+}, 500);
   }
 
   render() {
     return (
       <div>
-        <YouTube videoId={this.state.videoId} onReady={this.onReady} />
+        <YouTube videoId={this.state.videoId} opts={this.opts} onReady={this.onReady} />
         <button onClick={this.onChangePrevVideo}>Prev</button>
-        <button onClick={this.onPlayVideo} className={this.state.event_map.play ? "invisible" : ""}>Play</button>
-        <button onClick={this.onPauseVideo} className={!this.state.event_map.play ? "invisible" : ""}>Pause</button>
+        <button onClick={this.onPlayVideo} className={this.state.event_map.playing ? "invisible" : ""}>Play</button>
+        <button onClick={this.onPauseVideo} className={!this.state.event_map.playing ? "invisible" : ""}>Pause</button>
         <button onClick={this.onChangeNextVideo}>Next</button>
       </div>
     );
   }
 }
-
 
 
 
