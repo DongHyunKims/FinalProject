98fc41fa8479d5e5566500d11dbf357981c4d5da
kimseyoon <kimse7533@naver.com>
2017-05-11 17:40:30 +0900

    Update callback hell promise로 리팩토링
    1. 키워드를 검색해서 데이터를 받아오는 부분이 callback hell 발생. 해당 부분은 ES6의 Promise를 사용하여 리팩토링 하였음

--- mainApp/src/components/mainListComponent/searchListComponent/SearchListItem.js
+++ mainApp/src/components/mainListComponent/searchListComponent/SearchListItem.js
@@ -1,6 +1,7 @@
 import React from "react"
 
 import timeago from 'timeago.js';
+import utility from '../../../utility/utility';
 
 class SearchListItem extends React.Component{
   constructor(){
@@ -14,7 +15,7 @@ class SearchListItem extends React.Component{
 
     this.changePublishedAtData = this.changePublishedAtData.bind(this);
     this.clickAddButton = this.clickAddButton.bind(this);
-    this.changeDuration = this.changeDuration.bind(this);
+    //this.changeDuration = this.changeDuration.bind(this);
 
     this.showPopupVideo = this.showPopupVideo.bind(this);
   }
@@ -30,51 +31,6 @@ class SearchListItem extends React.Component{
       this.props.delSelectedVideo(videoId);
     }
     this.setState({isClickedAddBtn : !this.state.isClickedAddBtn});
-  }
-
-  changeDuration(duration){
-    let sec = 0,
-        min = 0,
-        hour = 0,
-        time = 0,
-        str = "";
-//hour
-    time = duration / 1000;
-    if(time / 3600 >= 1){
-      hour = Math.floor(time / 3600);
-      if(hour < 10){
-        str = str.concat("0"+hour+":");
-      }else{
-        str = str.concat(hour+":");
-      }
-    }else{
-      hour = 0;
-      str = str.concat("00:")
-    }
-//min
-    time = time % 3600;
-    if(time / 60  >= 1){
-      min = Math.floor(time / 60);
-      if(min < 10){
-        str = str.concat("0"+min+":");
-      }else{
-        str = str.concat(min+":");
-      }
-    }else{
-      min = 0;
-      str = str.concat("00:")
-    }
-//sec
-    sec = time % 60;
-    if(sec < 10){
-      str = str.concat("0"+sec);
-    }else{
-      str = str.concat(sec);
-    }
-
-    //console.log(str)
-    //console.log("hour = "+hour+" min = "+min+" sec = "+sec)
-    return str
   }
 
 
@@ -117,7 +73,7 @@ class SearchListItem extends React.Component{
         <div className="itemCont">
           <p className="title" onClick={this.showPopupVideo}>{data.title}</p>
           <p className="info">
-            <span className="duration">{this.changeDuration(data.duration)}</span>
+            <span className="duration">{utility.changeDuration(data.duration)}</span>
             <span className="date">{this.changePublishedAtData(data.publishedAt)}</span>
             <span className="viewCount">{data.viewCount} Views</span>
           </p>
